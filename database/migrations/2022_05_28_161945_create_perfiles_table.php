bf5cc067cf4ec741a36c12f04f6bce33a5215129
OrtizDroguett <j.ortizdroguett21@gmail.com>
2022-05-28 13:53:47 -0400

    se creo controlador y se modifico migración, también se agrego ruta

--- database/migrations/2022_05_28_161945_create_perfiles_table.php
+++ database/migrations/2022_05_28_161945_create_perfiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePerfilTable extends Migration
+class CreatePerfilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,14 @@ class CreatePerfilTable extends Migration
      */
     public function up()
     {
-        Schema::create('perfil', function (Blueprint $table) {
+        Schema::create('perfiles', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('rut');
+            $table->string('nombre');
+            $table->string('apellidoPaterno');
+            $table->string('apellidoMaterno');
+            $table->integer('telefono');
+            $table->string('comuna');
             $table->timestamps();
         });
     }
@@ -27,6 +32,6 @@ class CreatePerfilTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('perfil');
+        Schema::dropIfExists('perfiles');
     }
 }
