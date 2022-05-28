6a45837266c97d8214045301c928fb59143d1f76
OrtizDroguett <j.ortizdroguett21@gmail.com>
2022-05-28 16:51:38 -0400

    Se creo javascript de perfil, se agrego jquery, se añadio bootstrap, layouts y rutas nuevas

---
+++ resources/views/perfil/tabs/formularioPerfil.blade.php
@@ -0,0 +1,46 @@
+<!-- Fila -->
+<div class="row">
+    <!-- Rut-->
+    <div class="col-6">
+        <label for="rutTxt">Rut</label>
+        <input type="rutTxt" class=" form-control">
+    </div>
+    <!-- Nombre-->
+    <div class="col-6">
+        <label for="nombreTxt">Nombre</label>
+        <input type="nombreTxt" class=" form-control">
+    </div>
+</div>
+<!-- Fila -->
+<div class="row">
+    <!-- Apellido paterno-->
+    <div class="col-6">
+        <label for="apellidoPaternoTxt">Apellido Paterno</label>
+        <input type="apellidoPaternoTxt" class=" form-control">
+    </div>
+    <!-- Apellido materno-->
+    <div class="col-6">
+        <label for="apellidoPaternoTxt">Apellido Materno</label>
+        <input type="apellidoMaternoTxt" class=" form-control">
+    </div>
+</div>
+<!-- Fila -->
+<div class="row">
+    <!-- Telefono-->
+    <div class="col-6">
+        <label for="telefonoTxt">Telefono</label>
+        <input type="telefonoTxt" class=" form-control">
+    </div>
+    <!-- Comuna-->
+    <div class="col-6">
+    <label for="comunaSelect">Comuna</label>
+        <select name="" id="comunaSelect" class=" form-control">
+            <option value="La florida">La florida</option>
+            <option value="Cerrillos">Cerrillos</option>
+            <option value="Estación Central">Estación Central</option>
+            <option value="Maipú">Maipú</option>
+            <option value="Ñuñoa">Ñuñoa</option>
+        </select>
+    </div>
+</div>
+<button type="button" class="btn btn-outline-success mt-3" id="button">Guardar</button>
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
 
 Route::group(['prefix'=>'perfil'], function(){
-    Route::get('/', [PerfilController::class,'index']);
+    Route::get('/', [PerfilController::class,'index'])->name('perfil.index');
 });
